4ed58dda0a06b671ea33bda9f2343a9bb90252ec
naltai64 <damien@d-riviere.fr>
2018-07-20 17:57:00 +0200

    Ajout de la partie 'Qui suis-je ?' et 'Dernières publications'

--- index.php
+++ index.php
@@ -71,53 +71,186 @@
 
         </header>
 
+        <!-- DEBUT CAROUSEL -->
         <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
+
+            <!-- Indicateur du nombre de slide -->
             <ol class="carousel-indicators">
                 <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
                 <li data-target="#carouselIndicators" data-slide-to="1"></li>
                 <li data-target="#carouselIndicators" data-slide-to="2"></li>
             </ol>
+
+            <!-- Contient les sliders et leurs description -->
             <div class="carousel-inner">
+
                 <div class="carousel-item active" id="slider_1">
-                    <img class="d-block w-100" src="public/img/slider1.jpg" alt="First slide">
+                    <!-- Image 1 -->
+                    <img class="img-fluid" src="public/img/slider1.jpg" alt="First slide">
+                    <!-- Description slider 1 -->
                     <div class="container">
                         <div class="carousel-caption text-left">
-                            <h1>Example headline.</h1>
-                            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                            <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                            <h1 class="style_title">Bienvenue sur le blog de Jean Forteroche !</h1>
+                            <p class="style_texte">Pour mon prochain roman "Billet simple pour l'Alaska", j'ai souhaité pouvoir publier les chapitres en ligne sur mon blog. Un chapitre sortira par mois, bonne lecture !</p>
+                            <p><a class="btn btn-lg btn-primary btn-carousel" href="#" role="button">Voir le dernier chapitre</a></p>
                         </div>
                     </div>
                 </div>
+
                 <div class="carousel-item" id="slider_2">
-                    <img class="d-block w-100" src="public/img/slider2.jpg" alt="Second slide">
+                    <!-- Image 2 -->
+                    <img class="img-fluid" src="public/img/slider2.jpg" alt="Second slide">
+                    <!-- Description slider 2 -->
                     <div class="container">
                         <div class="carousel-caption text-left">
-                            <h1>Example headline.</h1>
-                            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                            <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                            <h2 class="style_title">Prochain roman</h2>
+                            <p class="style_texte">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                            <p><a class="btn btn-lg btn-primary btn-carousel" href="#" role="button">Voir prochain projet</a></p>
                         </div>
                     </div>
                 </div>
+
                 <div class="carousel-item" id="slider_3">
-                    <img class="d-block w-100" src="public/img/slider3.jpg" alt="Third slide">
+                    <!-- Image 2 -->
+                    <img class="img-fluid" src="public/img/slider3.jpg" alt="Third slide">
+                    <!-- Description slider 2 -->
                     <div class="container">
                         <div class="carousel-caption text-left">
-                            <h1>Example headline.</h1>
-                            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                            <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                            <h2 class="style_title">Me contacter</h2>
+                            <p class="style_texte">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                            <p><a class="btn btn-lg btn-primary btn-carousel" href="#" role="button">Contact</a></p>
                         </div>
                     </div>
                 </div>
+
             </div>
+
+            <!-- Chevron gauche -->
             <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
                 <i class="fas fa-angle-left"></i>
                 <span class="sr-only">Previous</span>
             </a>
+
+            <!-- Chevron droit -->
             <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
                 <i class="fas fa-angle-right"></i>
                 <span class="sr-only">Next</span>
             </a>
+
         </div>
+        <!-- FIN CAROUSEL -->
+
+        <div class="banner-responsive">          
+            <!-- 1ère image -->
+            <img class="img-fluid" src="public/img/slider1.jpg" alt="First slide">
+            <h2 class="title-banner">Bienvenue sur le blog de Jean Forteroche !</h2>
+        </div>
+
+        <main role="main">
+            <section class="jumbotron text-center">
+                <div class="container">
+                    <h1>Qui suis-je ?</h1>
+                    <p class="text-jumbotron">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </section>
+
+            <div class="album py-5 bg-light">
+                <div class="container">
+                    <h1 class="title-jumbotron">Dernières publications :</h1>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card mb-4 box-shadow">
+                                <img class="card-img-top" src="public/img/thumbnail1.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <a class="btn btn-primary" href="#" role="button">Voir chapitre</a>
+                                        <small class="text-muted">9 mins</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card mb-4 box-shadow">
+                                <img class="card-img-top" src="public/img/thumbnail2.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <a class="btn btn-primary" href="#" role="button">Voir chapitre</a>
+                                        <small class="text-muted">9 mins</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card mb-4 box-shadow">
+                                <img class="card-img-top" src="public/img/thumbnail3.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <a class="btn btn-primary" href="#" role="button">Voir chapitre</a>
+                                        <small class="text-muted">9 mins</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="card mb-4 box-shadow">
+                                <img class="card-img-top" src="public/img/thumbnail4.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <a class="btn btn-primary" href="#" role="button">Voir chapitre</a>
+                                        <small class="text-muted">9 mins</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card mb-4 box-shadow">
+                                <img class="card-img-top" src="public/img/thumbnail5.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <a class="btn btn-primary" href="#" role="button">Voir chapitre</a>
+                                        <small class="text-muted">9 mins</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card mb-4 box-shadow">
+                                <img class="card-img-top" src="public/img/thumbnail6.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <a class="btn btn-primary" href="#" role="button">Voir chapitre</a>
+                                        <small class="text-muted">9 mins</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <footer class="footer">
+            <div id="social-media">
+                <p class="text-footer">Suivez moi sur mes réseaux sociaux :</p>
+                <a href="https://www.facebook.com" title="Visitez ma page Facebook"><i class="fab fa-facebook-square style-logo"></i></a>
+                <a href="https://twitter.com/?lang=fr" title="Visitez ma page Twitter"><i class="fab fa-twitter style-logo"></i></a>
+                <a href="https://plus.google.com/discover" title="Visitez ma page Google +"><i class="fab fa-google-plus style-logo"></i></a>
+                <a href="https://fr.linkedin.com" title="Visitez ma page Youtube"><i class="fab fa-youtube style-logo"></i></a>
+            </div>
+        </footer>
         
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
