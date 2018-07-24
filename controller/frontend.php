5700bbf239f68643fc88e64c90ab08dda8def33c
naltai64 <damien@d-riviere.fr>
2018-07-24 18:32:39 +0200

    Ajout d'une fonction pour afficher la liste des chapitres et d'une autre pour afficher un chapitre en fonction de son ID

--- controller/frontend.php
+++ controller/frontend.php
@@ -3,12 +3,34 @@
 require_once('model/PostManager.php');
 
 /**
- * Fonction pour afficher les posts
+ * Fonction pour afficher les derniers posts sur la page d'accueil
+ *
+ */
+function listPostsIndex() {
+    $postManager = new PostManager();
+    $posts = $postManager->getPostsIndex();
+
+    require('view/frontend/indexView.php');
+}
+
+/**
+ * Fonction pour afficher la liste des posts
  *
  */
 function listPosts() {
     $postManager = new PostManager();
     $posts = $postManager->getPosts();
 
-    require('view/frontend/indexView.php');
+    require('view/frontend/alaskaBookView.php');
+}
+
+/**
+ * Fonction pour afficher un post en fonction de son ID
+ *
+ */
+function post() {
+    $postManager = new PostManager();
+    $post = $postManager->getPost($_GET['id']);
+
+    require('view/frontend/chapterView.php');
 }
