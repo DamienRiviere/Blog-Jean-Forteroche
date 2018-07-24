7b10883d6466e4b3ce86451cb202074c3290d612
naltai64 <damien@d-riviere.fr>
2018-07-24 18:31:30 +0200

    Routeur: Ajout d'affichage de la liste des chapitres et d'un chapitre en fonction de son ID

--- index.php
+++ index.php
@@ -2,4 +2,19 @@
 
 require('controller/frontend.php');
 
-listPosts();
+if (isset($_GET['action'])) {
+    // Si action = listPosts alors on affiche la liste des chapitres
+    if ($_GET['action'] == 'listPosts') {
+        listPosts();
+    }
+    // Si action = post alors on affiche le chapitre en fonction de son ID
+    else if ($_GET['action'] == 'post') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            post();
+        }
+    }
+}
+// Sinon on affiche la page d'accueil
+else {
+    listPostsIndex();
+}
