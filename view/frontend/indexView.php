37b1330c9e0948f4701f3b3c021fa53a7f297e15
naltai64 <damien@d-riviere.fr>
2018-07-24 18:36:10 +0200

    Ajout des liens vers les chapitres sur les thumbnails

--- view/frontend/indexView.php
+++ view/frontend/indexView.php
@@ -98,10 +98,11 @@
                     <div class="card mb-4 box-shadow">
                         <img class="card-img-top" src="public/img/thumbnail1.jpg" alt="Card image cap">
                         <div class="card-body">
-                            <h5 class="card-title"><?= htmlspecialchars($data['title']) ?></h5>
-                            <p class="card-text"><?= nl2br(htmlspecialchars(substr($data['content'], 0, 250))) ?></p>
+                            <h5 class="card-title"><?= $data['title'] ?></h5>
+                            <p class="card-text"><?= nl2br(substr($data['content'], 0, 250)) ?></p>
                             <div class="d-flex justify-content-between align-items-center">
-                                <a class="btn btn-primary" href="#" role="button">Voir chapitre</a>
+                                <!-- Bouton pour afficher le chapitre -->
+                                <a class="btn btn-primary" href="index.php?action=post&amp;id=<?= $data['id'] ?>" role="button">Voir chapitre</a>
                                 <small class="text-muted"><?= $data['creation_date_fr'] ?></small>
                             </div>
                         </div>
