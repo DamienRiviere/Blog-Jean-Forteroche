94a35887a21c5b2f911f34497924713917ef7eba
naltai64 <damien@d-riviere.fr>
2018-07-24 18:33:51 +0200

    Ajout de deux fonctions getPosts(Récupération des chapitres) et getPost(Récupération d'un chapitre en fonction de son ID)

--- model/PostManager.php
+++ model/PostManager.php
@@ -5,14 +5,41 @@ require_once("Manager.php");
 class PostManager extends Manager {
 
     /**
-     * Fonction pour récupérer les posts
+     * Fonction pour récupérer les derniers posts de la page d'accueil
+     *
+     * @return $req
+     */
+    public function getPostsIndex() {
+        $db = $this->dbConnect();
+        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 3');
+        return $req;
+    }
+
+    /**
+     * Fonction pour récupérer les derniers posts
      *
      * @return $req
      */
     public function getPosts() {
         $db = $this->dbConnect();
-        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 6');
+        $req = $db->query('SELECT id, title, content, author, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date');
         return $req;
     }
+
+    /**
+     * Fonction qui récupère un post en fonction de son ID
+     *
+     * @param [type] $postId
+     * @return $post
+     */
+    public function getPost($postId) {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id, title, content, author, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+        $req->execute(array($postId));
+        $post = $req->fetch();
+        return $post;
+    }
+
+    
 
 }
