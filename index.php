c0c7155cda614240a5170c12a070da2ced3ce5ff
naltai64 <damien@d-riviere.fr>
2018-07-25 17:25:57 +0200

    Ajout de l'affichage de la page contact dans le routeur

--- index.php
+++ index.php
@@ -3,15 +3,23 @@
 require('controller/frontend.php');
 
 if (isset($_GET['action'])) {
-    // Si action = listPosts alors on affiche la liste des chapitres
+    // Action = listPosts alors on affiche la liste des chapitres
     if ($_GET['action'] == 'listPosts') {
         listPosts();
     }
-    // Si action = post alors on affiche le chapitre en fonction de son ID
+    // Action = post alors on affiche le chapitre en fonction de son ID
     else if ($_GET['action'] == 'post') {
         if (isset($_GET['id']) && $_GET['id'] > 0) {
             post();
         }
+    }
+    // Action = contact alors on affiche la page du formulaire de contact
+    else if ($_GET['action'] == 'contact') {
+        showContact();
+    }
+    // TEST MAIL
+    else if ($_GET['action'] == 'test') {
+        actionForm();
     }
 }
 // Sinon on affiche la page d'accueil
