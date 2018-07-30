2ffb8d59c419866e8ee7291fb8294491837594fd
naltai64 <damien@d-riviere.fr>
2018-07-30 17:32:27 +0200

    Création d'une fonction pour vérifier les informations à la connexion d'un utilisateur

--- controller/backend.php
+++ controller/backend.php
@@ -1,8 +1,10 @@
 <?php
 
 require_once('model/Register.php');
+require_once('model/Connection.php');
 
 use \Openclassrooms\Projet_4\Model\Register;
+use \Openclassrooms\Projet_4\Model\Connection;
 
 /**
  * Fonction qui vérifie les données envoyés à l'inscription
@@ -39,7 +41,7 @@ function checkRegister() {
                             {
                                 $pass_hache = password_hash($password, PASSWORD_DEFAULT);
                                 $insertMembers = $register->newAccount($pseudo, $pass_hache, $email);
-                                $error = "Votre compte a bien été créé !";
+                                $error = "Votre compte a bien été créé ! <a href=\"index.php?action=connection\">Me connecter</a>";
                             }
                             else
                             {
@@ -74,4 +76,82 @@ function checkRegister() {
     }   
 
     require('view/frontend/registerView.php');
+}
+
+/**
+ * Fonction qui vérifie les données à la connection
+ *
+ */
+function checkConnection() {
+
+    $connection = new Connection();
+
+    if(isset($_POST['formconnection'])) {
+
+        $emailConnect = htmlspecialchars($_POST['emailconnect']);
+        $passwordConnect = $_POST['passwordconnect'];
+
+        if(!empty($emailConnect) AND !empty($passwordConnect))
+        {
+            if(filter_var($emailConnect, FILTER_VALIDATE_EMAIL))
+            {
+                $checkUser = $connection->checkUser($emailConnect);
+                $checkPass = password_verify($passwordConnect, $checkUser['pass']);
+
+                if($checkPass)
+                {
+                    $_SESSION['id'] = $checkUser['id'];
+                    $_SESSION['pseudo'] = $checkUser['pseudo'];
+                    $_SESSION['email'] = $checkUser['email'];
+                    header("Location: index.php?action=profile&id=" . $_SESSION['id']);
+                }
+                else
+                {
+                    $error = "Mauvais identifiant ou mot de passe !";
+                }
+            }
+            else
+            {
+                $error = "Votre adresse mail n'est pas valide !";
+            }
+        }
+        else
+        {
+            $error = "Tous les champs doivent être complétés !";
+        }
+
+    }
+
+    require('view/frontend/connectionView.php');
+
+}
+
+/**
+ * Fonction pour se déconnecter de son profil
+ *
+ */
+function disconnection() {
+    session_start();
+    $_SESSION = array();
+    session_destroy();
+    header("Location: index.php");
+}
+
+/**
+ * Fonction pour éditer son profil
+ *
+ * @return void
+ */
+function editProfile() {
+    
+    if(isset($_SESSION['id']))
+    {
+
+    }
+    else
+    {
+        header("Location: index.php?action=connection");
+    }
+
+    require('view/frontend/editProfileView.php');
 }
