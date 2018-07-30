6a1f4c2d523991976d0a64b4b23420350231521e
naltai64 <damien@d-riviere.fr>
2018-07-30 17:38:17 +0200

    Création de 3 vues pour la connexion, le profil et la modification du profil

---
+++ view/frontend/editProfileView.php
@@ -0,0 +1,10 @@
+<!-- View de la page d'édition d'un profil -->
+<?php ob_start(); ?>
+
+<div class="container-page bg-light">
+
+</div>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php');
