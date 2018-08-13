232124766f47f8b19c5a2081823dbe7248e4e5c3
naltai64 <damien@d-riviere.fr>
2018-08-13 17:12:11 +0200

    Modification de chapitre

---
+++ controllers/ControllerEditchapter.php
@@ -0,0 +1,89 @@
+<?php
+session_start();
+
+require_once('views/View.php');
+
+class ControllerEditchapter {
+
+    private $_view;
+    private $_chapterManager;
+
+    protected $id;
+    protected $newTitle;
+    protected $newAuthor;
+    protected $newContent;
+
+    public $error;
+    public $msg;
+
+    /**
+     * Constructeur ou l'on controle qu'il n'y est pas plusieurs paramètre dans l'URL 
+     * et on affiche une exception si c'est le cas
+     * sinon on lance la fonction editionChapter
+     *
+     * @param [type] $url
+     */
+    public function __construct($url)
+    {
+        if(isset($url) && count($url) > 1)
+        {
+            echo "Error 404";
+        }
+        else if(isset($_GET['url']) == 'editchapter' AND empty($_SESSION))
+        {
+            throw new Exception('Page introuvable');
+        }
+        else if(isset($_POST['submit']))
+        {
+            $this->id = $_GET['id'];
+            $this->updateChapter($this->id);
+            $this->editChapter($this->id);
+        }
+        else
+        {
+            $this->editChapter($_GET['id']);
+        }
+    }
+
+    /**
+     * Fonction qui affiche le chapitre a modifié
+     *
+     * @param [type] $id
+     */
+    private function editChapter($id)
+    {
+        $this->_chapterManager = new ChapterManager;
+        $chapter = $this->_chapterManager->getChapter($id);
+
+        $this->_view = new View('Editchapter');
+        $this->_view->generate(array(
+            'chapter' => $chapter,
+            'error' => $this->error,
+            'msg' => $this->msg
+        ));
+    }
+
+    /**
+     * Fonction qui met à jour le chapitre
+     *
+     * @param [type] $id
+     */
+    private function updateChapter($id)
+    {
+        if(isset($_POST['new_chapter_title']) AND !empty($_POST['new_chapter_title']) AND isset($_POST['new_chapter_content']) AND !empty($_POST['new_chapter_content']))
+        {
+            $this->newTitle = $_POST['new_chapter_title'];
+            $this->newAuthor = $_SESSION['pseudo'];
+            $this->newContent = $_POST['new_chapter_content'];
+
+            $this->_chapterManager = new ChapterManager;
+            $updateChapter = $this->_chapterManager->updateChapter($this->id, $this->newTitle, $this->newAuthor, $this->newContent);
+            $this->msg = "Votre chapitre a été mis à jour !";
+        }
+        else
+        {
+            $this->error = "Veuillez remplir tous les champs !";
+        }
+    }
+
+}
