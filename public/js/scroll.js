d89c196dfa71abb4a9fb76ab188274aaf406eaf7
naltai64 <damien@d-riviere.fr>
2018-08-15 13:40:44 +0200

    Script pour le scrolling des ancres

---
+++ public/js/scroll.js
@@ -0,0 +1,10 @@
+/* Scrolling des boutons "Accueil" et "Réservation" */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
